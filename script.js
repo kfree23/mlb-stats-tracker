f78f6551b22b4924e0f001beb30305eec5ec80b0
kfree23 <krystalfreeman50@gmail.com>
2026-06-23 23:55:17 -0500

    create two seperate functions and clear input boxes

--- script.js
+++ script.js
@@ -30,49 +30,62 @@ function createPlayer({ name, team, position }) {
     };
 
     players.push(player);
+    playerInput.value = '';
+    teamInput.value = '';
+    positionInput.value = '';
 
-    const card = document.createElement('div');
-    card.classList.add('card-styles', 'p-3', 'player-grid');
-    card.classList.add('card');
-
-    const infoContainer = document.createElement('div');
-    infoContainer.classList.add('card-styles', 'text-positioning', 'flex');
-
-    const playerName = document.createElement('h3');
-    playerName.textContent = name;
-
-    const teamName = document.createElement('p');
-    teamName.textContent = team;
-
-    const positionElement = document.createElement('p');
-    positionElement.textContent = position;
-
-    card.appendChild(playerName);
-    infoContainer.appendChild(teamName);
-    infoContainer.appendChild(positionElement);
-
-    const hr = document.createElement('hr');
-
-    const statCount = document.createElement('div');
-    statCount.classList.add('stat-count', 'flex');
-
-    const statPositioning = document.createElement('div');
-    statPositioning.classList.add('stat-positioning');
-
-    const statNum = document.createElement('p');
-    statNum.textContent = 0;
-
-    const statName = document.createElement('p');
-    statName.textContent = "HR";
-
-    statPositioning.appendChild(statNum);
-    statPositioning.appendChild(statName);
-    statCount.appendChild(statPositioning);
-
-    card.appendChild(infoContainer);
-    card.appendChild(hr);
-    card.appendChild(statCount);
-    
-
-    playerCards.appendChild(card);
+    renderPlayer();
 }
+
+function renderPlayer() {
+
+    players.forEach(player => {
+        playerCards.innerHTML = '';
+
+        const card = document.createElement('div');
+        card.classList.add('card-styles', 'p-3', 'player-grid');
+        card.classList.add('card');
+
+        const infoContainer = document.createElement('div');
+        infoContainer.classList.add('card-styles', 'text-positioning', 'flex');
+
+        const playerName = document.createElement('h3');
+        playerName.textContent = player.name;
+
+        const teamName = document.createElement('p');
+        teamName.textContent = player.team;
+
+        const positionElement = document.createElement('p');
+        positionElement.textContent = player.position;
+
+        card.appendChild(playerName);
+        infoContainer.appendChild(teamName);
+        infoContainer.appendChild(positionElement);
+
+        const hr = document.createElement('hr');
+
+        const statCount = document.createElement('div');
+        statCount.classList.add('stat-count', 'flex');
+
+        const statPositioning = document.createElement('div');
+        statPositioning.classList.add('stat-positioning');
+
+        const statNum = document.createElement('p');
+        statNum.textContent = 0;
+
+        const statName = document.createElement('p');
+        statName.textContent = "HR";
+
+        statPositioning.appendChild(statNum);
+        statPositioning.appendChild(statName);
+        statCount.appendChild(statPositioning);
+
+        card.appendChild(infoContainer);
+        card.appendChild(hr);
+        card.appendChild(statCount);
+
+
+        playerCards.appendChild(card);
+    });
+};
+
